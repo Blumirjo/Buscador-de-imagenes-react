6a35d7c547faca0551eb302c46a538f67178d131
Sebastian Gatica <expandiendoartemusical@gmail.com>
2022-04-26 16:52:09 -0300

    buscador-de-imagenes

---
+++ buscador.imagenes/src/App.js
@@ -0,0 +1,50 @@
+import { useState } from 'react';
+import {Formik, Form, Field} from 'formik';
+import './header.css';
+import './content.css';
+import './article.css';
+
+const App = () => {
+  const [photos, setPhotos] = useState([])
+  const open = url => window.open(url)
+  console.log({photos})
+  return (
+    <div>
+      <header>
+        <Formik
+        initialValues={ {search: ''} }
+        onSubmit={async values => {
+          const response= await fetch(`https://api.unsplash.com/search/photos?per_page=20&query=${values.search}`, {
+            headers: {
+              'Authorization': 'Client-ID J7F6Vf6rrhFbF8NXO_ZAP5qzkF6sfAXfgA0sg2kFHNw'
+            }
+          } )
+
+          const data = await response.json()
+          //llamar a api unsplash
+             setPhotos(data.results)
+        }}
+        >
+
+<Form>
+  <Field name='search' />
+</Form>
+
+
+        </Formik>
+      </header>
+      <div className='container'>
+         <div className='center'>
+            {photos.map(photo =>
+            <article key={photos.id} onClick={()=> open(photo.links.html)} >
+              <img src={photo.urls.regular}/>
+              <p>{[photo.description, photo.alt_description].join(' - ')}</p>
+            </article> )}
+         </div>
+      </div>
+
+    </div>
+  );
+}
+
+export default App;
